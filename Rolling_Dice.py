2ed6bf9fdc871d861a7a06af5e93b16f2fd212ad
Kaloyan Stoyanov <kalo.stoyanov@gmail.com>
2018-04-15 19:27:07 +0200

    .

--- Rolling_Dice.py
+++ Rolling_Dice.py
@@ -1,1 +1,16 @@
+import random
 
+min = int(input("Please enter the minimum number: "))
+max = int(input("Please enter the maximum number: "))
+
+number_to_guess = random.randrange(min,max)
+
+guess_number = int(input("Please try to guess the number: "))
+while guess_number != number_to_guess:
+    print(f"{guess_number} is not correct!")
+    if guess_number < number_to_guess:
+        print("Try something higher.")
+    else:
+        print("Try something lower.")
+    guess_number = int(input("Please try again: "))
+print("This is correct!!!")
